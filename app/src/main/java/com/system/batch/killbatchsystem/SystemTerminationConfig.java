2ffd172a3e5f2d2572c21ac9eef9aa1538a0c153
taemin6536 <xoals6536@naver.com>
2025-08-19 09:22:21 +0900

    first commit

---
+++ app/src/main/java/com/system/batch/killbatchsystem/SystemTerminationConfig.java
@@ -0,0 +1,10 @@
+package com.system.batch.killbatchsystem;
+
+import com.system.batch.killbatchsystem.config.BatchConfig;
+import org.springframework.context.annotation.Import;
+
+@Import(BatchConfig.class)
+public class SystemTerminationConfig {
+
+
+}
